0ac5c1bad7a7ebfbad9a3ff5d6e9ba041a1d2527
Miden <midenthedev@gmail.com>
2026-09-25 23:17:57 -0700

    Make getter for levelsystem in registry

--- src/main/java/us/to/midensthings/serverLevels/data/Registries.java
+++ src/main/java/us/to/midensthings/serverLevels/data/Registries.java
@@ -1,10 +1,15 @@
 package us.to.midensthings.serverLevels.data;
 
 import us.to.midensthings.serverLevels.ServerLevels;
+import us.to.midensthings.serverLevels.systems.LevelSystem;
 
 public class Registries {
 
     private static final ServerLevels plugin = ServerLevels.getPlugin(ServerLevels.class);
 
-    public static LevelSystemRegistry levelSystemRegistry = plugin.getLevelSystemRegistry();
+    private static LevelSystemRegistry levelSystemRegistry = plugin.getLevelSystemRegistry();
+
+    public static LevelSystem getLevelSystem(String name) {
+        return levelSystemRegistry.getLevelSystem(name);
+    }
 }
